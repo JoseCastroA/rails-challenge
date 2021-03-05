020ba5bcdb7e13da86aa92a0d162a6fbc4dfd945
Jose Castro Arias <jose.castro@kommit.co>
2021-03-04 20:24:12 -0500

    Add report function to generate CSV report

--- challenge/app/controllers/employees_controller.rb
+++ challenge/app/controllers/employees_controller.rb
@@ -2,4 +2,14 @@ class EmployeesController < ApplicationController
   def index
     @employees = Employee.all
   end
+
+  def report
+    respond_to do |format|
+      format.json { redirect_to root_path }
+      format.xml { redirect_to root_path }
+      format.csv do
+        send_data Employee.build_csv, filename: "#{Time.now.strftime('%Y-%m-%d_%I:%M:%S%p')}_employees_.csv", status: 200
+      end
+    end
+  end
 end
