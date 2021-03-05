8673436d07f8888b1baec63b17ca6b7cb126e9e3
Jose Castro Arias <jose.castro@kommit.co>
2021-03-04 20:25:05 -0500

    Add config to routes, CSV class and factory_bot

--- challenge/config/routes.rb
+++ challenge/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :employees
+  root "employees#index"
+  resources :employees do
+    get :report, on: :collection
+  end
 end
